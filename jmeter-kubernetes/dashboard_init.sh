40a8d6a594d99fa390d54b939411e23227190f6f
Sakari Hoisko <sakari.hoisko@northcode.fi>
2021-04-01 07:44:22 +0300

    Upgraded grafana to Grafana v7.5.1

--- jmeter-kubernetes/dashboard_init.sh
+++ jmeter-kubernetes/dashboard_init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -xe
 working_dir=`pwd`
 
 #Get namesapce variable
@@ -13,20 +13,25 @@ echo "Creating Influxdb jmeter Database"
 ##influxdb_status=`kubectl get po -n $tenant | grep influxdb-jmeter | awk '{print $2}' | grep Running
 
 influxdb_pod=`kubectl get po -n $tenant | grep influxdb-jmeter | awk '{print $1}'`
-kubectl exec -ti -n $tenant $influxdb_pod -- influx -execute 'CREATE DATABASE jmeter'
+kubectl exec -ti -n $tenant $influxdb_pod -- influx -execute 'CREATE DATABASE jmeterdb'
 
 ## Create the influxdb datasource in Grafana
 
-echo "Creating the Influxdb data source"
-grafana_pod=`kubectl get po -n $tenant | grep jmeter-grafana | awk '{print $1}'`
+
 
 ## Make load test script in Jmeter master pod executable
 
-#Get Master pod details
-#master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+echo "Get Master pod details"
+master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
 #kubectl exec -ti -n $tenant $master_pod -- cp -r /load_test /jmeter/load_test
 #kubectl exec -ti -n $tenant $master_pod -- chmod 755 /jmeter/load_test
 
 ##kubectl cp $working_dir/influxdb-jmeter-datasource.json -n $tenant $grafana_pod:/influxdb-jmeter-datasource.json
 
-kubectl exec -ti -n $tenant $grafana_pod -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
+echo "Creating the grafana datasource"
+grafana_pod=`kubectl get po -n $tenant | grep jmeter-grafana | awk '{print $1}'`
+echo "Add infludb datasource:"
+kubectl exec -ti -n $tenant $master_pod -- curl 'http://admin:admin@jmeter-grafana:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeterdb","user":"admin","password":"admin"}'
+
+# GET DATASOURCES:
+kubectl exec -ti -n $tenant $master_pod -- curl 'http://admin:admin@jmeter-grafana:3000/api/datasources' -X GET
